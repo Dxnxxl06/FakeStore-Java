fe93efd015f26593e60655d2335d4f3d7984d234
keiner <keinergomez8687@gmail.com>
2025-09-04 10:50:02 -0500

    feat: :sparkles: Fetch de productos

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ const sortProducts = document.getElementById("sortProducts");
 
 let products = [];
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Fetch productos
+async function fetchProducts() {
+    try {
+      const res = await fetch(API_URL);
+      products = await res.json();
+      renderProducts(products);
+      loadCategories();
+      renderCart();
+    } catch (error) {
+      console.error("Error al cargar productos:", error);
+    }
+  }
