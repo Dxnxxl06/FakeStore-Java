435ebf6cb8aaf5ab09481f97c495ad1e91794424
keiner <keinergomez8687@gmail.com>
2025-09-04 10:48:53 -0500

    feat: :sparkles: Consumo de API

---
+++ app.js
@@ -0,0 +1,7 @@
+const API_URL = "https://fakestoreapi.com/products";
+const productsContainer = document.getElementById("productsContainer");
+const cartItems = document.getElementById("cartItems");
+const cartTotal = document.getElementById("cartTotal");
+const searchInput = document.getElementById("searchInput");
+const filterCategory = document.getElementById("filterCategory");
+const sortProducts = document.getElementById("sortProducts");
